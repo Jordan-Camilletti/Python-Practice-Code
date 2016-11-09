29c5b828f6d4a9bf555557692778ce75003acc26
Jordan Camilletti <jordan.camilletti@gmail.com>
2016-11-09 08:18:29 -0600

    Update Logic 2: noTeenSum.py

--- Logic 2: noTeenSum.py
+++ Logic 2: noTeenSum.py
@@ -1,15 +1,11 @@
 """Given 3 int values, a b c, return their sum.
 However, if any of the values are in the range 13..19 inclusive then that value counts as 0, except 15 and 16 do not count as a teens."""
 
-public int noTeenSum(int a, int b, int c) {
-  if((a>12&&a<15)||(a>16&&a<20)){
-    a=0;
-  }
-  if((b>12&&b<15)||(b>16&&b<20)){
-    b=0;
-  }
-  if((c>12&&c<15)||(c>16&&c<20)){
-    c=0;
-  }
-  return a+b+c;
-}
+def no_teen_sum(a, b, c):
+  if(a>12 and a<15)or(a>16 and a<20):
+    a=0
+  if(b>12 and b<15)or(b>16 and b<20):
+    b=0
+  if(c>12 and c<15)or(c>16 and c<20):
+    c=0
+  return a+b+c
