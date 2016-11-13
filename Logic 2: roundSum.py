ce535adb38db8e836c5bbe313af5521f2d782eef
Jordan Camilletti <jordan.camilletti@gmail.com>
2016-11-12 20:51:54 -0600

    Delete Logic 2: roundSum.py

--- Logic 2: roundSum.py
+++
@@ -1,4 +0,0 @@
-"""For this problem, we'll round an int value up to the next multiple of 10 if its rightmost digit is 5 or more,
-so 15 rounds up to 20. 
-Alternately, round down to the previous multiple of 10 if its rightmost digit is less than 5, so 12 rounds down to 10. 
-Given 3 ints, a b c, return the sum of their rounded values. """
