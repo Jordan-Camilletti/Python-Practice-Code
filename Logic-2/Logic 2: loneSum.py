176c4c3fcdd89d18b09ff96389a21cb94a8a56d6
Jordan Camilletti <jordan.camilletti@gmail.com>
2016-12-07 13:15:48 -0600

    Update Logic 2: loneSum.py

--- Logic-2/Logic 2: loneSum.py
+++ Logic-2/Logic 2: loneSum.py
@@ -1,16 +1,13 @@
 """Given 3 int values, a b c, return their sum.
 However, if one of the values is the same as another of the values, it does not count towards the sum."""
 
-public int loneSum(int a, int b, int c){
-  if(a==b&&b==c){
-    return 0;
-  }else if(a==b){
-    return c;
-  }else if(a==c){
-    return b;
-  }else if (b==c){
-    return a;
-  }else{
-    return (a+b+c);
-  }
-}
+def lone_sum(a, b, c):
+  if(a==b and a==c):
+    return 0
+  elif(a==c):
+    return b
+  elif(b==c):
+    return a
+  elif(a==b):
+    return c
+  return a+b+c
