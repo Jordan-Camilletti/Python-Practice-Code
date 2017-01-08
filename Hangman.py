b4532212df8d17bbac5ddae889f2e16c6942a4c1
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-01-07 20:34:41 -0600

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -1,5 +1,13 @@
 """This uses a randomly selected word or a user input word and plays hangman with it."""
+
 from random import randint
 
 wordBank=["test1","test2","test3"]
-print(wordBank[randint(0,len(wordBank)-1)])
+word=(wordBank[randint(0,len(wordBank)-1)])
+tries=6
+while(tries!=0):
+	tries-=1
+	str=""
+	for x in range(len(word)):
+		str+="_"
+	print(str)
