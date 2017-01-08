53d260dd61c09631effb9c08b92be0a7605f4994
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-01-07 22:11:21 -0600

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -8,12 +8,13 @@ def maymay(letter, word, arr):
 			arr[x]=letter
 	return arr
 
-wordBank=["test1","test2","test3"]
+wordBank=["test1","test2","test3"]#Add an option to allow user to use word bank or use own word
 wrd=(wordBank[randint(0,len(wordBank)-1)])
 arr=["_"]*(len(wrd))
 tries=5
-while(tries!=0):
+while(tries!=0):#Break if they guess word
 	choice=input('Enter a letter')
 	tries-=1
 	arr=maymay(choice,wrd,arr)
 	print(arr)
+#Create win/loss message
