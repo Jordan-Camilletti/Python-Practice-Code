dc454ddbd61866f75dec4d25a4b247eac697f909
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-01-08 14:22:31 -0600

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -2,11 +2,17 @@
 
 from random import randint
 
-def maymay(letter, word, arr):
+def tryLetter(letter, word, arr):
 	for x in range(len(word)):
 		if(letter==word[x]):
 			arr[x]=letter
 	return arr
+
+def containt(letter, word):
+	for x in word:
+		if(x==letter):
+			return True
+	return False
 
 wordBank=["test1","test2","test3"]
 if(input('Do you want to use your own word? Y/N\n')=="Y"):
@@ -15,13 +21,16 @@ else:
 	wrd=(wordBank[randint(0,len(wordBank)-1)])
 arr=["_"]*(len(wrd))
 tries=5
-while(tries!=0):#Break if they guess word
+while(tries!=0):
 	choice=input("Enter a letter\n")
 	tries-=1
-	arr=maymay(choice,wrd,arr)
+	arr=tryLetter(choice,wrd,arr)
 	for x in arr:
 		if x==choice:
 			tries+=1
 			break
 	print(arr," tries left:", tries)
-#Create a win/loss message
+	if(!containt("_",arr)):
+		print("You win!")
+		break
+print("You lose!")
