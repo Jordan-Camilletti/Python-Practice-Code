d96fb2c83f62ad45979fb7eba46d572f6eecaa01
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-02-20 10:32:43 -0600

    Update Biggestes and Smallestes.py

--- Biggestes and Smallestes.py
+++ Biggestes and Smallestes.py
@@ -1,6 +1,12 @@
 from collections import deque
 import heapq
 
+def largest(num, col):
+	newLst=deque(maxlen=num)
+	for x in sorted(col):
+		newLst.append(x)
+	return(newLst)
+	
 def smallest(num, col):
 	newLst=deque(maxlen=num)
 	heap=list(col)
@@ -8,8 +14,7 @@ def smallest(num, col):
 	for x in range(num):
 		newLst.append(heapq.heappop(heap))
 	return(newLst)
-	
 
 col=[3,6,8,1,5,8,3,9,4,2]
-#print(largest(2,list))
+print(largest(2,col))
 print(smallest(2,col))
