2f8915fc316031746e84a4d7fb446a390db5bda4
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-04-29 15:46:28 -0500

    Update Pascal Triangle Recursion.py

--- Pascal Triangle Recursion.py
+++ Pascal Triangle Recursion.py
@@ -1,4 +1,14 @@
 """
 """
 
-def tri():
+def tri(topLevel, currLevel, triangle):
+	triangle=triangle+"*"
+	if(topLevel==1):
+		triangle=triangle+"\n"
+		return triangle
+	if(currLevel==1):
+		triangle=triangle+"\n"
+		return tri(topLevel-1,topLevel-1,triangle)
+
+
+print(tri(5, 5,""))
