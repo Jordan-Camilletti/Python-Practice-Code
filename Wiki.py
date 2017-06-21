5f51a7c54b94d81a9dd2e669acf7969ab9e9afba
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-06-21 13:20:47 -0500

    Update Wiki.py

--- Wiki.py
+++ Wiki.py
@@ -6,9 +6,11 @@ import urllib.request
 print("Enter starting prompt")
 prompt=input()
 route=[]
-while(prompt not "Philosophy"):
+while(prompt!="Philosophy"):
 	route.append(prompt)
 	url="https://en.wikipedia.org/wiki/"+prompt
+	if(url=="https://en.wikipedia.org/wiki/Philosophy"):
+		break
 	prompt=""
 	lnk_found=False
 	webbrowser.open(url)
@@ -19,21 +21,29 @@ while(prompt not "Philosophy"):
 			lnk_found=True
 			break
 	lnk_found=False
+	par=False
 	line=lne.decode("utf-8")
 	for x in range(len(line)):
-		if(line[x-15:x]=='<a href="/wiki/'):
-			newWrd=line[x:]
-			for n in range(len(newWrd)):
-				try:
-					if(newWrd[n]!='"'):
-						prompt+=newWrd[n]
-					else:
-						newWrd=""
-				except IndexError:
-					print(prompt)
-					lnk_found=True
-					break
+		if(line[x]=="("):
+			par=True
+		if(line[x]==")"):
+			par=False
+		if(not par):
+			if(line[x-15:x]=='<a href="/wiki/'):
+				newWrd=line[x:]
+				for n in range(len(newWrd)):
+					try:
+						if(newWrd[n]!='"'):
+							prompt+=newWrd[n]
+						else:
+							newWrd=""
+					except IndexError:
+						lnk_found=True
+						break
 		if(lnk_found):
 			break
 	print(prompt)
+	if(prompt in route):
+		print("Endless loop encountered!")
+		break
 print(route)
