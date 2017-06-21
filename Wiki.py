c184f46588a1b8096507078249d0f4060ef55d9b
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-06-21 13:07:54 -0500

    Update Wiki.py

--- Wiki.py
+++ Wiki.py
@@ -6,45 +6,34 @@ import urllib.request
 print("Enter starting prompt")
 prompt=input()
 route=[]
-route.append(prompt)
-url="https://en.wikipedia.org/wiki/"+prompt
-lnk=""
-lnk_found=False
-#webbrowser.open(url)
-page=urllib.request.urlopen(url)
-while(page.readline() or lnk_found==False):
-	lne=page.readline()
-	if(b'<p>' in lne and b'<a href="/wiki/' in lne):
-		lnk_found=True
-		break
-lnk_found=False
-line=lne.decode("utf-8")
-for x in range(len(line)):
-	if(line[x-15:x]=='<a href="/wiki/'):
-		newWrd=line[x:]
-		for n in range(len(newWrd)):
-			try:
-				if(newWrd[n]!='"'):
-					lnk+=newWrd[n]
-				else:
-					newWrd=""
-			except IndexError:
-				print(lnk)
-				lnk_found=True
-				break
-	if(lnk_found):
-		break
-
+while(prompt not "Philosophy"):
+	route.append(prompt)
+	url="https://en.wikipedia.org/wiki/"+prompt
+	prompt=""
+	lnk_found=False
+	webbrowser.open(url)
+	page=urllib.request.urlopen(url)
+	while(page.readline() or lnk_found==False):
+		lne=page.readline()
+		if(b'<p>' in lne and b'<a href="/wiki/' in lne):
+			lnk_found=True
+			break
+	lnk_found=False
+	line=lne.decode("utf-8")
+	for x in range(len(line)):
+		if(line[x-15:x]=='<a href="/wiki/'):
+			newWrd=line[x:]
+			for n in range(len(newWrd)):
+				try:
+					if(newWrd[n]!='"'):
+						prompt+=newWrd[n]
+					else:
+						newWrd=""
+				except IndexError:
+					print(prompt)
+					lnk_found=True
+					break
+		if(lnk_found):
+			break
+	print(prompt)
 print(route)
-
-"""lne=b'<p><b>Water</b> is a transparent and nearly colorless <a href="/wiki/Chemical_substance" title="Chemical substance">chemi'
-line=lne.decode("utf-8")
-for x in range(len(line)):
-	if(line[x-15:x]=='<a href="/wiki/'):
-		newWrd=line[x:]
-		for n in range(len(newWrd)):
-			if(newWrd[n]!='"'):
-				lnk+=newWrd[n]
-			else:
-				print(lnk)
-				break"""
