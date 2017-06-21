7a89b3ca81ee3d5b50680266548bc594626703ee
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-06-21 12:56:38 -0500

    Update Wiki.py

--- Wiki.py
+++ Wiki.py
@@ -9,21 +9,31 @@ route=[]
 route.append(prompt)
 url="https://en.wikipedia.org/wiki/"+prompt
 lnk=""
+lnk_found=False
 #webbrowser.open(url)
 page=urllib.request.urlopen(url)
-while(page.readline()):
+while(page.readline() or lnk_found==False):
 	lne=page.readline()
-	if(b'<p>' in line and b'<a href="/wiki/' in lne):
-		line=lne.decode("utf-8")
-		for x in range(len(line)):
-			if(line[x-15:x]=='<a href="/wiki/'):
-			newWrd=line[x:]
-			for n in range(len(newWrd)):
+	if(b'<p>' in lne and b'<a href="/wiki/' in lne):
+		lnk_found=True
+		break
+lnk_found=False
+line=lne.decode("utf-8")
+for x in range(len(line)):
+	if(line[x-15:x]=='<a href="/wiki/'):
+		newWrd=line[x:]
+		for n in range(len(newWrd)):
+			try:
 				if(newWrd[n]!='"'):
 					lnk+=newWrd[n]
 				else:
-					print(lnk)
-					break
+					newWrd=""
+			except IndexError:
+				print(lnk)
+				break
+				lnk_found=True
+		if(lnk_found):
+			break
 
 print(route)
 
