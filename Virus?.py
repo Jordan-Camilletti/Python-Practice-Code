5edd098f96e1550d93cbfaae6eaf878fa4f042df
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-07-07 15:45:36 -0500

    Create Virus?.py

---
+++ Virus?.py
@@ -0,0 +1,12 @@
+"""I guess this could be classified as a very basic virus
+DO NOT run this on anything other than a virtual machine
+Even on a virtual machine, I still wouldn't want to run this
+Finding all of this content probably put me on so many watchlists."""
+
+import webbrowser
+webbrowser.open("www.google.com")
+imgurUrls=["0CUFPuU.gifv","4JeXPYL.jpg","OUnesDL.jpg","Iq2CH.jpg","dD7yNfY.jpg","a/GRqUA","pKZCq.jpg","EaZSYU1.gifv","LdRVqSL.jpg","tbB9ktj.jpg"]
+redditUrls=["84yqig40xm7z.png"]
+youtubeUrls=["watch?v=19eFnrSf41A&feature=youtu.be&t=5m39s"]
+liveleakUrls=["view?i=461_1360105026&safe_mode=off","view?i=bb4_1283184704&safe_mode=off"]
+otherUrls=["http://33.media.tumblr.com/5743e899731d3604cfbc1871ef64f9dc/tumblr_njk3p6hCUh1unvl7to1_500.gif"]
