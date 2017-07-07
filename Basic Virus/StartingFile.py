44f70df8dcf824e33148436ce86275abd3b67598
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-07-07 18:44:00 -0500

    Update StartingFile.py

--- Basic Virus/StartingFile.py
+++ Basic Virus/StartingFile.py
@@ -3,4 +3,9 @@ DO NOT run this on anything unless you want your search history ruined
 Finding all of this content probably put me on so many watchlists."""
 
 import BadStuff
-BadStuff.start()
+try:
+	BadStuff.start(2)
+except(KeyboardInterrupt, SystemExit):
+	print("There's no brakes on the web spam train!")
+	BadStuff.start(1)
+	BadStuff.start(1)
