90e789c26f2848620a46f639f622b1136dad01b5
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-07-07 18:14:17 -0500

    Update and rename Starting FIle.py to StartingFIle.py

--- Basic Virus/StartingFIle.py
+++ Basic Virus/StartingFIle.py
@@ -1,3 +1,6 @@
 """I guess this could be classified as a very basic virus
 DO NOT run this on anything unless you want your search history ruined
 Finding all of this content probably put me on so many watchlists."""
+
+import BadStuff
+BadStuff.start()
