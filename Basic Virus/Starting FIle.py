d65819ede196e41fe29f5613f214a9cb33066a7c
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-07-07 18:08:50 -0500

    Update Starting FIle.py

--- Basic Virus/Starting FIle.py
+++ Basic Virus/Starting FIle.py
@@ -1,2 +1,3 @@
-"""
-"""
+"""I guess this could be classified as a very basic virus
+DO NOT run this on anything unless you want your search history ruined
+Finding all of this content probably put me on so many watchlists."""
