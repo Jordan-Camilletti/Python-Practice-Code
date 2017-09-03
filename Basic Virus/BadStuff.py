373292cad026c565a483577e75d70408869a4a85
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-09-02 22:16:58 -0500

    Update BadStuff.py

--- Basic Virus/BadStuff.py
+++ Basic Virus/BadStuff.py
@@ -1,6 +1,7 @@
 """I guess this could be classified as a very basic virus
 DO NOT run this on anything unless you want your search history ruined
-Finding all of this content probably put me on so many watchlists."""
+Finding all of this content probably put me on so many watchlists.
+Created by Jordan Camilletti"""
 
 import webbrowser
 import time
