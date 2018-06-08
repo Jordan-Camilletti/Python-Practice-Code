7a9d17f600cc04293f253b4fee7a6698dfdef6e1
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-06-07 22:38:27 -0500

    Update AutoCipher.py

--- AutoCipher.py
+++ AutoCipher.py
@@ -33,3 +33,39 @@ while(choice.upper()!="EXIT"):
 				else:
 					cText+="᏿"
 			print(cText)
+"""This is a basic reolace letter cipher"""
+
+alphC=[]#Cipher 'alphabet'
+alphN=[]#Normal 'alphabet'
+choice=""
+cText=""
+nText=""
+while(choice.upper()!="EXIT"):
+	print("\n"+alphN+" "+alphC)
+	choice=input("Input, Output, or Exit?\n")
+	if(choice.upper()=="INPUT"):
+		cText=input("Enter ciphered text.\n").upper()
+		nText=input("Enter normal text.\n").upper()
+		for n in range(len(cText)):
+			if(cText[n] not in alphC):
+				alphC.append(cText[n])
+				alphN.append(nText[n])
+	elif(choice.upper()=="OUTPUT"):
+		if(input("Cipher -> Normal (1)\nNormal -> Cipher(2)\n")=="2"):
+			cText=input("Enter normal text.\n")
+			nText=""
+			for n in cText:
+				if(n in alphC):
+					nText+=alphN[alphC.index(n)]
+				else:
+					nText+="᏿"
+			print(nText)
+		else:
+			nText=input("Enter ciphered text.\n")
+			cText=""
+			for n in nText:
+				if(n in alphN):
+					cText+=alphC[alphN.index(n)]
+				else:
+					cText+="᏿"
+			print(cText)
