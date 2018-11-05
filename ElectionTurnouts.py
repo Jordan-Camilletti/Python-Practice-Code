db563d36c9e03905e93c66881f3583f3a384d880
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-11-04 23:14:34 -0600

    Update ElectionTurnouts.py

--- ElectionTurnouts.py
+++ ElectionTurnouts.py
@@ -4,11 +4,10 @@ import webbrowser
 import urllib
 from urllib.request import urlopen
 
-#Starting year is 1792
 #The voter turnout percentage is always the first number on line 52
 percents=[11.6]#11.6% turnout in 1788-1789 election
 
-for year in range(1792,2020,4):
+for year in range(1792,2020,4):#Starting year is 1792, ending year is 2016
   url="https://en.wikipedia.org/wiki/United_States_presidential_election,_"+str(year)
   #webbrowser.open(url)
   print(list(enumerate(urllib.request.urlopen(url)))[51])#Gets the specific line
