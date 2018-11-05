8c0565ab50d3edd966d263d49e86de83d3e38a7c
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-11-04 23:11:22 -0600

    Update ElectionTurnouts.py

--- ElectionTurnouts.py
+++ ElectionTurnouts.py
@@ -10,6 +10,6 @@ year=1792
 percents=[]
 
 for year in range(1792,2020,4):
-url="https://en.wikipedia.org/wiki/United_States_presidential_election,_"+str(year)
-#webbrowser.open(url)
-print(list(enumerate(urllib.request.urlopen(url)))[51])#Gets the specific line
+  url="https://en.wikipedia.org/wiki/United_States_presidential_election,_"+str(year)
+  #webbrowser.open(url)
+  print(list(enumerate(urllib.request.urlopen(url)))[51])#Gets the specific line
