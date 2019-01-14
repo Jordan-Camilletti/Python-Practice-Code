9d17519255b24130528d1db9f5ce4dbe46aa78b8
Jordan Camilletti <jordan.camilletti@gmail.com>
2019-01-13 22:44:25 -0600

    Web page data now successfully gathered

--- BPM.py
+++ BPM.py
@@ -1,15 +1,16 @@
 import webbrowser
-import urllib
-from urllib.request import urlopen, Request
+import urllib.request
 
-headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+header = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
        'Accept-Encoding': 'none',
        'Accept-Language': 'en-US,en;q=0.8',
        'Connection': 'keep-alive'}
 url="https://tunebat.com/Search?q=Space+Oddity+"
-urllib.request.urlopen(url)
+page=urllib.request.urlopen(urllib.request.Request(url,headers=header))
+print(page.read())
+#urllib.request.urlopen(url)
 #page=Request(url, headers)
 #print(urlopen(page).read().decode('utf-8'))
 """while(page.readline()):
