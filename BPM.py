8d916d2e03288d74dbc438f7817e92de21f980ee
Jordan Camilletti <jordan.camilletti@gmail.com>
2019-01-13 20:30:48 -0600

    Removed redundant imports

--- BPM.py
+++ BPM.py
@@ -1,11 +1,13 @@
 import webbrowser
-import urllib
-from urllib.request import urlopen
+#import urllib
+from urllib.request import urlopen, Request
 
+headers = {‘User-Agent’: ‘Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3’}
 url="https://tunebat.com/Search?q=Space+Oddity+"
-page=urllib.request.urlopen(url)
-while(page.readline()):
+page=Request(url=url, headers=headers)
+print(urlopen(page).read())
+"""while(page.readline()):
 	lne=page.readline()
-	print(lne)
+	print(lne)"""
 #f=urllib.request.urlopen(url)
 #print(f.read())
