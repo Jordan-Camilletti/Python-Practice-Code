dd5deb2ca00371251d4fe61c141e0f7b488eecb1
Jordan Camilletti <jordan.camilletti@gmail.com>
2019-01-16 23:01:40 -0600

    Update BPM.py

--- BPM.py
+++ BPM.py
@@ -8,10 +8,11 @@ header={'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML
        'Accept-Language': 'en-US,en;q=0.8',
        'Connection': 'keep-alive'}
 url="https://songbpm.com/space-oddity?q=Space%20Oddity%20"#Using this URL to test for now
+BPM=0
 page=urllib.request.urlopen(urllib.request.Request(url,headers=header))
 print(page.read())
 with page.read() as pge:
-	
+	BPM=
 #urllib.request.urlopen(url)
 #page=Request(url, headers)
 #print(urlopen(page).read().decode('utf-8'))
