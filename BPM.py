b94c985197b3d92d643e6ddbbbaa4a253d4f223f
Jordan Camilletti <jordan.camilletti@gmail.com>
2019-01-16 23:02:24 -0600

    Added song variables

--- BPM.py
+++ BPM.py
@@ -7,6 +7,8 @@ header={'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML
        'Accept-Encoding': 'none',
        'Accept-Language': 'en-US,en;q=0.8',
        'Connection': 'keep-alive'}
+song1=input("Enter Song 1: ")
+song2=""
 url="https://songbpm.com/space-oddity?q=Space%20Oddity%20"#Using this URL to test for now
 BPM=0
 page=urllib.request.urlopen(urllib.request.Request(url,headers=header))
