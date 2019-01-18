a8081b7369ebd05fcd7ace23b7cdee6577cfc39f
Jordan Camilletti <jordan.camilletti@gmail.com>
2019-01-17 21:50:49 -0600

    Got rid of some of the older commented out algorithms

--- BPM.py
+++ BPM.py
@@ -2,7 +2,7 @@ import webbrowser
 import urllib.request
 
 def makeNewUrl(song,url="https://songbpm.com/"):
-	return("")
+	return("https://songbpm.com/space-oddity?q=Space%20Oddity%20")
 
 header={'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -10,18 +10,16 @@ header={'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML
        'Accept-Encoding': 'none',
        'Accept-Language': 'en-US,en;q=0.8',
        'Connection': 'keep-alive'}
-song1=(input("Enter Song 1: ")).replace(" ","-").toLowerCase()
+song1=input("Enter Song 1: ").lower()
 song2=""
 #url="https://songbpm.com/space-oddity?q=Space%20Oddity%20"#Using this URL to test for now
 url=makeNewUrl(song1)
 BPM=0
 page=urllib.request.urlopen(urllib.request.Request(url,headers=header))
 print(page.read())
-with page.read() as pge:
-	BPM=
+#with page.read() as pge:
+#	BPM=
+
 #urllib.request.urlopen(url)
 #page=Request(url, headers)
 #print(urlopen(page).read().decode('utf-8'))
-"""while(page.readline()):
-	lne=page.readline()
-	print(lne)"""
