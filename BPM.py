9e91c3a9db53e08dabc85967499557fdc0e957a4
Jordan Camilletti <jordan.camilletti@gmail.com>
2019-01-22 23:57:59 -0600

    Added more imports

--- BPM.py
+++ BPM.py
@@ -1,6 +1,6 @@
 import re
 import webbrowser
-from urllib.requestimport urlopen
+from urllib.request import urlopen
 
 header={'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
